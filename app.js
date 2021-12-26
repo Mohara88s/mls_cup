76e2fbfd773927c68214dfb563f89e015ae986dc
Mohara88s <mohara88s@gmail.com>
2021-12-26 21:08:58 +0200

    fix fonts & add js on video

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const refs = {
     mlsCupCloseBtn: document.querySelector('.mlsCupCloseButton'),
     mlsCup: document.querySelector('.mlsCup'),
-  };
+    videoBackdrop: document.querySelector('.videoBackdrop'),
+    hotSpotBtn: document.querySelector('.hotSpotButton'),
+    videoCloseBtn: document.querySelector('.videoCloseButton'),
+    videoPlayBtn: document.querySelector('.videoPlayButton'),  
+    video: document.querySelector('.video'),  
+};
   
-  refs.mlsCupCloseBtn.addEventListener('click', mlsCupClos)
-  function mlsCupClos() {
+  refs.mlsCupCloseBtn.addEventListener('click', onMlsCupCloseBtnClick)
+  function onMlsCupCloseBtnClick() {
     refs.mlsCup.setAttribute("hidden", true);
+  }
+
+  refs.hotSpotBtn.addEventListener('click', onHotSpotBtnClick)
+  function onHotSpotBtnClick() {
+      refs.videoBackdrop.classList.remove('is-hidden')
+  }
+
+  refs.videoCloseBtn.addEventListener('click', onVideoCloseBtnClick)
+  function onVideoCloseBtnClick() {
+      refs.videoBackdrop.classList.add('is-hidden')
+      refs.video.pause() 
+  }
+
+  refs.videoPlayBtn.addEventListener('click', onVideoPlayBtnClick)
+  function onVideoPlayBtnClick() {
+      if (refs.video.paused) {
+        refs.video.play() 
+      } else {
+        refs.video.pause()  
+      }
   }
