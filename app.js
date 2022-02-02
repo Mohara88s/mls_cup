5235ef4366c569eb2cd9292d9bceaad4687b7680
Mohara88s <mohara88s@gmail.com>
2022-02-02 15:59:49 +0200

    remade on GSAP

--- app.js
+++ app.js
@@ -39,8 +39,21 @@ const refs = {
       }
   }
 
-
-
   window.addEventListener("load", function(){
-    refs.mlsCup.classList.remove('is-hidden')
+    var tl = gsap.timeline();
+    tl.to(".mlsCup",{duration: 1, opacity:1})
+      .to(".overlay",{duration: 2, y:0})
+      .to(".confetti",{duration: 3.5, yPercent:-145, ease: "back.out(1.15)",}, "-=1")
+      .to(".leftDoor",{duration: 1.8, x:0}, "-=1.5")
+      .to(".rightDoor",{duration: 1.8, x:0}, "-=1.8")
+      .to(".sloganList__item:first-child",{duration: 0.8, y:0})
+      .to(".sloganList__item:nth-child(2)",{duration: 1, y:0}, "-=0.2")
+      .to(".sloganList__item:last-child",{duration: 1.2, y:0}, "-=0.2")
+      .to(".leftDoor__emblem",{duration: 1, opacity:1, scale:1})
+      .to(".rightDoor__emblem",{duration: 1, opacity:1, scale:1}, "-=1")
+      .to(".banner ",{duration: 0.01, opacity:1})
+      .to(".sloganList",{duration: 1.5, yPercent:-300}, "+=2")
+      .to(".leftDoor",{duration: 2, xPercent:-100}, "-=1")
+      .to(".rightDoor",{duration: 2, xPercent:100}, "-=2")
 });
+
